764616bcd45c35a16275a24efb5a426d24b5a04d
= <celotts@gmail.com>
2019-12-08 12:51:47 -0300

    .

--- server/routes/ciudad.router.js
+++ server/routes/ciudad.router.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const ForecastIo = require('forecastio');
+const weather = new ForecastIo('a5e036bc49458c5f7bb593e87668f9a8');
 
 const ciudadController = require('../controllers/ciudad.controller');
 
